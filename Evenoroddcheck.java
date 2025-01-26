e7768a73e573f0fd41e52feb9d341d7c52996fa7
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 13:04:37 +0530

    Add files via upload

---
+++ Evenoroddcheck.java
@@ -0,0 +1,16 @@
+public class Evenoroddcheck{
+    public static void main(String[] args) {
+        int number=5;
+        if(number%2==0){
+            System.out.println("the number is even");
+         } else{
+            System.out.println("the number is odd");
+         }
+        }
+    }
+        
+
+         
+    
+        
+    
